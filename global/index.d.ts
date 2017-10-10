6f87ab7636a10832a0ab9ca0aea9448600227f3f
Damien Lespiau <damien.lespiau@gmail.com>
2017-10-10 18:53:38 +0100

    Fix 'creted' typo (#43)

--- global/index.d.ts
+++ global/index.d.ts
@@ -15582,7 +15582,7 @@ declare namespace _ {
      * If a property name is provided iteratee the created "_.property" style callback returns
      * the property value of the given element.
      *
-     * If a value is also provided for thisArg the creted "_.matchesProperty" style callback returns
+     * If a value is also provided for thisArg the created "_.matchesProperty" style callback returns
      * true for elements that have a matching property value, else false;.
      *
      * If an object is provided for iteratee the created "_.matches" style callback returns true
